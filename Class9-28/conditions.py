6e40af9063e4f2146c861b8579853820a040062b
adachepally <akshayd01@gmail.com>
2019-09-28 14:09:40 -0400

    class work

---
+++ Class9-28/conditions.py
@@ -0,0 +1,13 @@
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+if(a > b):
+    print("True")
+else:
+    print("False")
+
+# > greater than
+# < less than
+# <= less than or equal to
+# >= greater than or equal to
+# == equals operator
+# = assignment operator
--- Class9-28/sortevenodd.py
+++ Class9-28/sortevenodd.py
@@ -1,1 +1,4 @@
 #create a program to sort even and odd numbers
+# 1. Prompt user for a number
+# 2. Evaluate if number is even or odd
+# 3. Return even or odd
