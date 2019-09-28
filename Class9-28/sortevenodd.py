3b7c3d44174db8de7020eb5ae390ce802e9dced3
adachepally <akshayd01@gmail.com>
2019-09-28 14:20:48 -0400

    sort even odd

--- Class9-28/sortevenodd.py
+++ Class9-28/sortevenodd.py
@@ -2,3 +2,11 @@
 # 1. Prompt user for a number
 # 2. Evaluate if number is even or odd
 # 3. Return even or odd
+
+number = int(input("Please Enter a Number"))
+if(number%2 == 0):
+    print("This is an even number")
+else:
+    print("This is an odd number")
+
+
