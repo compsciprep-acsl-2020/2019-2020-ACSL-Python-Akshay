0ecd360974874dfba0c1931d96cd953d95679d12
adachepally <akshayd01@gmail.com>
2019-10-05 14:29:02 -0400

    calculator answer with functionality

--- class10-05/calculator.py
+++ class10-05/calculator.py
@@ -20,7 +20,13 @@ def division(num1, num2):
 num1 = int(input("Enter the First Number"))
 num2 = int(input("Enter the Second Number"))
 
-print(add(num1,num2))
-print(sub(num1,num2))
-print(multiply(num1,num2))
-print(division(num1,num2))
+option = int(input("Enter 1 for Addition \n Enter 2 for Subtration \n Enter 3 for Multiplication \n Enter 4 for Division"))
+
+if (option == 1):
+    print("The sum is: ", add(num1,num2))
+elif (option == 2):
+    print("The difference is: ", sub(num1,num2))
+elif (option == 3):
+    print("The product is: ", multiply(num1,num2))
+elif (option == 4):
+    print("the quotient is: ", division(num1,num2))
