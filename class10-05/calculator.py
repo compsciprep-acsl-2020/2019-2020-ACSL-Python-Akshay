1a130b6d58d5b77923a1d38c7bc95f3e38384341
adachepally <akshayd01@gmail.com>
2019-10-05 14:17:40 -0400

    calculator answer

--- class10-05/calculator.py
+++ class10-05/calculator.py
@@ -4,7 +4,23 @@
 #make an individual function to add, subtract, multiply and divide
 #return from each function
 #template for add function
-'''
-def add(number 1, number 2):
-    return number 1 + number 2
-'''
+
+def add(num1, num2):
+    return (num1+num2)
+
+def sub(num1, num2):
+    return (num1-num2)
+
+def multiply(num1, num2):
+    return (num1*num2)
+
+def division(num1, num2):
+    return (num1/num2)
+
+num1 = int(input("Enter the First Number"))
+num2 = int(input("Enter the Second Number"))
+
+print(add(num1,num2))
+print(sub(num1,num2))
+print(multiply(num1,num2))
+print(division(num1,num2))
