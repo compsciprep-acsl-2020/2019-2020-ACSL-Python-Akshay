e637844e2197f141ee2c0dfb4bd4e2679b3b2365
adachepally <akshayd01@gmail.com>
2019-10-05 14:42:07 -0400

    Homework due on Saturday October 12th

---
+++ assigned Homework/HW due on October 12th/AreaandPerimeter.py
@@ -0,0 +1,15 @@
+'''
+#Create two programs
+One will be called perimeter.py
+The other will be called area.py
+Each program should have a function to calculate the area/perimeter of
+    1. Triangle
+    2. Square
+    3. Rectangle
+    4. Parallelogram
+    5. Trapezoid
+    6. Circle
+    7. Rhombus
+'''
+
+
